30751fc5fc808c1ec72863ff21582046538c6b1b
mymcworld3-droid <mymcworld3@gmail.com>
2025-08-21 09:27:28 +0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ const WS_URL = (location.protocol === 'https:' ? 'wss' : 'ws') + '://' + locatio
 const ws = new WebSocket(WS_URL);
 
 // ======================= Canvas / World =======================
-let WORLD = { width: 12000, height: 9000 };
+let WORLD = { width: 60000, height: 45000 };
 const camera = { x: 0, y: 0 };
 
 const canvas = document.getElementById('gameCanvas');
